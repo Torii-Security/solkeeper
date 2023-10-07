d4e45e84a0b2e878ea11c8982474f184f67441a4
meltedblocks <meltedblocks@gmail.com>
2023-10-07 13:31:17 +0200

    Add CLI

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Have Docker installed. (on mac M1 turn rosetta feature - https://collabnix.com/w
 
 ## Problems
 When deployed locally - IDL is not uploaded - so verify instruction not pass.
+
+## Usage
+### Get Deployed Program Hash Validate
+Get program hash and validate with local binary
+```
+npm start get-program-hash-validate -- --programId {PROGRAM_ID} --clusterUrl {CLUSTER_URL} --pathToProgram {LOCAL_BIN_PATH}
+```
+
+### Get Deployed Program Hash
+Get program hash from cluster
+```
+npm start get-program-hash -- --programId {PROGRAM_ID} --clusterUrl {CLUSTER_URL}
+```
--- app/for-auditors/src/cli.ts
+++ app/for-auditors/src/cli.ts
@@ -1,8 +1,8 @@
 import { Command } from 'commander';
 import fs from 'fs';
 import { sha256 } from 'js-sha256';
-import { PublicKey } from '@solana/web3.js';
 import { getBinFromChain, addPaddingToBuffer } from './lib';
+import { PublicKey } from '@solana/web3.js';
 
 const packageJson = require('../package.json');
 const version: string = packageJson.version;
@@ -10,32 +10,72 @@ const version: string = packageJson.version;
 const program = new Command();
 
 program
-  .version(version)
-  .name('my-command')
-  .option('-d, --debug', 'enables verbose logging', false)
-  .parse(process.argv);
+  .name('solana-verifier')
+  .description('Torii Solana Verifier tool')
+  .version(version);
 
-// Example usage:
-const programId = new PublicKey('Ait72SouqcsR3GwpfNwQDeDzPQHLdoG1BvL7qiFb6xHe');
-const binPath = '';
-const cluster = 'http://localhost:8899'; // or the Solana cluster URL you want to use
-void (async () => {
-  try {
-    const deployedBin = await getBinFromChain(programId, cluster);
-    const localBin = fs.readFileSync(binPath);
-    // The deployed program probably has zero bytes appended. The default is
-    // 2x the binary size in case of an upgrade.
-    const { buf1, buf2 } = addPaddingToBuffer(deployedBin, localBin);
+program
+  .command('get-program-hash-validate')
+  .description('Get program hash and validate with local binary')
+  .requiredOption('--programId <programId>', 'Program ID')
+  .requiredOption('--pathToProgram <pathToProgram>', 'Path to Program')
+  .requiredOption('--clusterUrl <clusterUrl>', 'Cluster URL')
+  .action(options => {
+    void (async () => {
+      const { pathToProgram, clusterUrl } = options;
+      const programId = new PublicKey(options.programId);
 
-    const areSame = buf1.equals(buf2);
+      try {
+        const deployedBin = await getBinFromChain(programId, clusterUrl);
+        const localBin = fs.readFileSync(pathToProgram);
+        // The deployed program probably has zero bytes appended. The default is
+        // 2x the binary size in case of an upgrade.
+        const { buf1, buf2 } = addPaddingToBuffer(deployedBin, localBin);
 
-    if (!areSame) {
-      throw new Error('Bin mismatch');
-    }
+        const areSame = buf1.equals(buf2);
 
-    const binHash = sha256(buf1);
-    console.log(binHash);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-})();
+        if (!areSame) {
+          throw new Error('Bin mismatch');
+        }
+
+        const binHash = sha256(buf1);
+        console.log(binHash);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    })();
+  });
+
+program
+  .command('get-program-hash')
+  .description('Get program hash from cluster')
+  .requiredOption('--programId <programId>', 'Program ID')
+  .requiredOption('--clusterUrl <clusterUrl>', 'Cluster URL')
+  .action(options => {
+    void (async () => {
+      const { clusterUrl } = options;
+      const programId = new PublicKey(options.programId);
+
+      try {
+        const deployedBin = await getBinFromChain(programId, clusterUrl);
+        const binHash = sha256(deployedBin);
+        console.log(binHash);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    })();
+  });
+
+program
+  .command('validate-program')
+  .description('Validate program')
+  .requiredOption('--pathToProgram <pathToProgram>', 'Path to Program')
+  .requiredOption('--clusterUrl <clusterUrl>', 'Cluster URL')
+  .action(options => {
+    console.log('Validating program...');
+    console.log('Path to Program:', options.pathToProgram);
+    console.log('Cluster URL:', options.clusterUrl);
+    // Implement your logic here for validating the program.
+  });
+
+program.parse(process.argv);

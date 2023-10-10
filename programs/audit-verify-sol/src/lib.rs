851c3d6b14fc733040120e42c5642ce3e50167fd
meltedblocks <meltedblocks@gmail.com>
2023-10-10 18:25:27 +0200

    Add logic to register auditor

--- programs/audit-verify-sol/src/lib.rs
+++ programs/audit-verify-sol/src/lib.rs
@@ -6,6 +6,23 @@ declare_id!("Ait72SouqcsR3GwpfNwQDeDzPQHLdoG1BvL7qiFb6xHe");
 pub mod audit_verify_sol {
     use super::*;
     pub const PLATFORM_CONFIG_SEED: &'static [u8; 8] = b"platform";
+    pub const AUDITOR_SEED: &'static [u8; 8] = b"auditors";
+    pub const FEE_VAULT: &'static [u8; 8] = b"feevault";
+
+    
+    #[error_code]
+    pub enum Errors {
+        #[msg("Max name length is 255")]
+        NameTooLarge,
+        #[msg("Max url length is 255")]
+        UrlTooLarge,
+        #[msg("Insufficient Balance")]
+        InsufficientBalance,
+        #[msg("Too early to deactivate")]
+        TooEarly,
+        #[msg("Overflow")]
+        Overflow
+    }
 
     pub fn initialize_audit(
         ctx: Context<Initialize>,
@@ -54,6 +71,109 @@ pub mod audit_verify_sol {
         platform_config.fee = fee;
         Ok(())
     }
+
+    pub fn withdraw_fee(
+        ctx: Context<WithdrawFee>,
+        bump: u8
+    ) -> Result<()> {
+        let fee_vault_info = &mut ctx.accounts.fee_vault_info;
+        let receiver = &mut ctx.accounts.receiver;
+
+        let withdraw_fee_instruction = anchor_lang::solana_program::system_instruction::transfer(&fee_vault_info.key(), &receiver.key, fee_vault_info.lamports());
+        anchor_lang::solana_program::program::invoke_signed(
+            &withdraw_fee_instruction,
+            &[
+                fee_vault_info.to_account_info(),
+                receiver.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[&[FEE_VAULT, &[bump]]]
+        )?;
+
+        Ok(())
+    }
+
+    pub fn initialize_auditor(
+        ctx: Context<InitializeAuditor>,
+        name: String,
+        url: String,
+    ) -> Result<()> {
+        let platform_config = &mut ctx.accounts.platform_config_info;
+        let auditor_info = &mut ctx.accounts.auditor_info;
+        let fee_vault_info = &mut ctx.accounts.fee_vault_info;
+        let auditor = &mut ctx.accounts.auditor;
+
+        let amount_to_transfer = platform_config.escrow_amount.checked_add(platform_config.fee).expect(&Errors::Overflow.to_string());
+
+        require!(name.len() < 255, Errors::NameTooLarge);
+        require!(url.len() < 255, Errors::UrlTooLarge);
+        require!(auditor.lamports() > amount_to_transfer, Errors::InsufficientBalance);
+
+        // Transfer escrow
+        let transfer_escrow_instruction = anchor_lang::solana_program::system_instruction::transfer(auditor.key, &platform_config.key(), platform_config.escrow_amount);
+        anchor_lang::solana_program::program::invoke(
+            &transfer_escrow_instruction,
+            &[
+                auditor.to_account_info(),
+                platform_config.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ]
+        )?;
+
+        let transfer_fee_instruction = anchor_lang::solana_program::system_instruction::transfer(auditor.key, &fee_vault_info.key(), platform_config.fee);
+        anchor_lang::solana_program::program::invoke(
+            &transfer_fee_instruction,
+            &[
+                auditor.to_account_info(),
+                fee_vault_info.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ]
+        )?;
+
+        auditor_info.owner = ctx.accounts.auditor.key();
+        auditor_info.escrow_amount = platform_config.escrow_amount;
+        auditor_info.registration_time = Clock::get()?.unix_timestamp;
+        auditor_info.name = name;
+        auditor_info.url = url;
+        auditor_info.is_active = true;
+        Ok(())
+    }
+
+    pub fn deactivate_auditor(
+        ctx: Context<DeactivateAuditor>,
+        bump: u8
+    ) -> Result<()> {
+        let platform_config = &mut ctx.accounts.platform_config_info;
+        let auditor_info = &mut ctx.accounts.auditor_info;
+        let auditor = &mut ctx.accounts.auditor;
+
+        let amount_to_transfer =  auditor_info.escrow_amount;
+        let time_now = Clock::get()?.unix_timestamp;
+        let registration_time = auditor_info.registration_time;
+        let timelock = platform_config.timelock;
+        
+        // Validate that timelock passes
+        require!(registration_time.checked_add(timelock).expect(&Errors::Overflow.to_string()) < time_now, Errors::TooEarly);
+
+        // Transfer escrow
+        let transfer_escrow_instruction = anchor_lang::solana_program::system_instruction::transfer(
+            &platform_config.key(), 
+            auditor.key, 
+            amount_to_transfer
+        );
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_escrow_instruction,
+            &[
+                platform_config.to_account_info(),
+                auditor.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[&[PLATFORM_CONFIG_SEED, &[bump]]]
+        )?;
+        auditor_info.is_active = false;
+        Ok(())
+    }
+    
 }
 
 #[account]
@@ -76,12 +196,71 @@ pub struct PlatformConfig {
     owner: Pubkey
 }
 
+#[account]
+#[derive(Default)]
+pub struct AuditorInfo {
+    name: String,
+    url: String,
+    owner: Pubkey,
+    registration_time: i64,
+    escrow_amount: u64,
+    is_verified: bool,
+    is_active: bool
+}
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = auditor, space = 8 + AuditInfo::INIT_SPACE)]
+    #[account(init, payer = auditor, space = 8 + std::mem::size_of::<AuditInfo>() + 8)]
     pub audit_info: Account<'info, AuditInfo>,
     #[account(mut)]
     pub auditor: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct InitializeAuditor<'info> {
+    #[account(
+        init, 
+        seeds = [AUDITOR_SEED, auditor.key().as_ref()], 
+        payer = auditor, 
+        space = 8 + std::mem::size_of::<AuditorInfo>() + 8,
+        bump
+    )]
+    pub auditor_info: Account<'info, AuditorInfo>,
+    #[account(mut)]
+    pub auditor: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [FEE_VAULT], 
+        bump
+    )]
+    pub fee_vault_info: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [PLATFORM_CONFIG_SEED],
+        bump
+    )]
+    pub platform_config_info: Account<'info, PlatformConfig>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DeactivateAuditor<'info> {
+    #[account(
+        seeds = [AUDITOR_SEED, auditor.key().as_ref()], 
+        constraint = auditor_info.is_active,
+        bump
+    )]
+    pub auditor_info: Account<'info, AuditorInfo>,
+    #[account(mut)]
+    pub auditor: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [PLATFORM_CONFIG_SEED],
+        bump
+    )]
+    pub platform_config_info: Account<'info, PlatformConfig>,
     pub system_program: Program<'info, System>,
 }
 
@@ -108,6 +287,27 @@ pub struct UpdatePlatform<'info> {
         bump
     )]
     pub platform_config_info: Account<'info, PlatformConfig>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawFee<'info> {
+    #[account(
+        seeds = [PLATFORM_CONFIG_SEED],
+        constraint = platform_config_info.owner == owner.key(),
+        bump
+    )]
+    pub platform_config_info: Account<'info, PlatformConfig>,
+    #[account(
+        mut,
+        seeds = [FEE_VAULT], 
+        bump
+    )]
+    pub fee_vault_info: AccountInfo<'info>,
+    #[account(mut)]
+    pub receiver: AccountInfo<'info>,
     #[account(mut)]
     pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
--- programs/audit-verify-sol/tests/normal_flow.rs
+++ programs/audit-verify-sol/tests/normal_flow.rs
@@ -13,6 +13,8 @@ use anchor_client::solana_sdk::{
 };
 
 pub const PLATFORM_CONFIG_SEED: &'static [u8; 8] = b"platform";
+pub const AUDITOR_SEED: &'static [u8; 8] = b"auditors";
+pub const FEE_VAULT: &'static [u8; 8] = b"feevault";
 
 #[tokio::test]
 async fn happy_flow() {
@@ -30,7 +32,7 @@ async fn happy_flow() {
     program.add_account(
         auditor.pubkey(),
         Account {
-            lamports: 1_000_000_000,
+            lamports: 100_000_000_000,
             ..Account::default()
         },
     );
@@ -45,6 +47,14 @@ async fn happy_flow() {
 
     let (platform_config_info, _) =
         Pubkey::find_program_address(&[PLATFORM_CONFIG_SEED], &audit_verify_sol::id());
+
+    let (auditor_info, _) = Pubkey::find_program_address(
+        &[AUDITOR_SEED, auditor.pubkey().as_ref()],
+        &audit_verify_sol::id(),
+    );
+
+    let (fee_vault_info, fee_acc_bump) =
+        Pubkey::find_program_address(&[FEE_VAULT], &audit_verify_sol::id());
 
     let escrow_amount = 5_000_000_000; // 1 SOL
     let fee = 100_000_000; // 0.1 SOL
@@ -71,6 +81,47 @@ async fn happy_flow() {
     };
 
     let mut init_tx = Transaction::new_with_payer(&[init], Some(&owner.pubkey()));
+
+    init_tx.partial_sign(&[&owner], recent_blockhash);
+
+    banks_client.process_transaction(init_tx).await.unwrap();
+
+    let name: String = String::from("Torii");
+    let url: String = String::from("https://torii.wtf");
+
+    let init_auditor = Instruction {
+        program_id: audit_verify_sol::id(),
+        data: audit_verify_sol::instruction::InitializeAuditor { name, url }.data(),
+        accounts: audit_verify_sol::accounts::InitializeAuditor {
+            platform_config_info,
+            auditor_info,
+            fee_vault_info,
+            auditor: auditor.pubkey(),
+            system_program: system_program::ID,
+        }
+        .to_account_metas(None),
+    };
+
+    let mut init_tx = Transaction::new_with_payer(&[init_auditor], Some(&auditor.pubkey()));
+
+    init_tx.partial_sign(&[&auditor], recent_blockhash);
+
+    banks_client.process_transaction(init_tx).await.unwrap();
+
+    let withdraw_fee = Instruction {
+        program_id: audit_verify_sol::id(),
+        data: audit_verify_sol::instruction::WithdrawFee { bump: fee_acc_bump }.data(),
+        accounts: audit_verify_sol::accounts::WithdrawFee {
+            platform_config_info,
+            fee_vault_info,
+            owner: owner.pubkey(),
+            receiver: owner.pubkey(),
+            system_program: system_program::ID,
+        }
+        .to_account_metas(None),
+    };
+
+    let mut init_tx = Transaction::new_with_payer(&[withdraw_fee], Some(&owner.pubkey()));
 
     init_tx.partial_sign(&[&owner], recent_blockhash);
 
